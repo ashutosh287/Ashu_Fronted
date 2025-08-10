be0f87593d625862e8d569742963d1da4dbeecba
ashu <ashutoshrajput280@gmail.com>
2025-08-10 18:48:23 +0530

    Add

--- vite.config.js
+++ vite.config.js
@@ -17,7 +17,7 @@ import tailwindcss from '@tailwindcss/vite'
 //         secure: false,
 //         rewrite: (path) => path.replace(/^\/api/, '')
 //       },
-      
+
 //     }
 //   }
 // });
@@ -28,6 +28,9 @@ export default defineConfig({
   server: {
     host: true,
     port: 5173,
+    allowedHosts: [
+      'fronted-vercel.onrender.com' // ✅ allow this host
+    ],
     proxy: {
       '/api': {
         target: 'http://192.168.1.6:5005',
@@ -37,8 +40,7 @@ export default defineConfig({
       },
     }
   }
-});
-
+})
 
 
 
