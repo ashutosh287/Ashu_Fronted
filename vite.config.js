d314d98a7e079e52f5bd1f5f42db6abb5b1ec442
ashu <ashutoshrajput280@gmail.com>
2025-08-14 12:13:02 +0530

    ok

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,9 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
+
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    host: true,
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5005', // Local backend
-        changeOrigin: true,
-        secure: false,
-        // Do NOT rewrite path so that /api/user works directly
-      },
-    },
-  },
-});
+})
